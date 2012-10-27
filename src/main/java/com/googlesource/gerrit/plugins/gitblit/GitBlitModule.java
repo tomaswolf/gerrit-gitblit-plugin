GerritCodeReview/plugins_gitblit

GitBlit integration into Gerrit as repo viewer.
GitBlit is plugged into the Gerrit Servlet Container with
integration of Gerrit authentication, authorisation and
Git repositories.

Optional auto-configuration using Gerrit Plugin
InitStep (requires https://gerrit-review.googlesource.com/#/c/38041/).
OR
Manual configuration by putting the following entries in
your $GERRIT_SITE/etc/gerrit.config:

[gitweb]
        type = custom
        url = plugins/gitblit-plugin-1.0-SNAPSHOT/
        linkname = GitBlit
        project = summary/${project}
        revision = commit/${project}/${commit}
        branch = log/${project}/${branch}
        filehistory = history/${project}/${branch}/${file}

Change-Id: Ie82658bcbbf308520d48b88057212a683aca16a7

src/main/java/com/googlesource/gerrit/plugins/gitblit/GitBlitModule.java
@@ -1,0 +1,53 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.gitblit;
+
+import org.eclipse.jgit.lib.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.gitblit.IUserService;
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.httpd.rpc.account.GroupDetailHandler.Factory;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.google.inject.servlet.ServletModule;
+import com.googlesource.gerrit.plugins.gitblit.app.GerritGitBlit;
+import com.googlesource.gerrit.plugins.gitblit.auth.GerritToGitBlitUserService;
+
+public class GitBlitModule extends ServletModule {
+  private static final Logger log = LoggerFactory.getLogger(GitBlitModule.class);
+
+  @Inject
+  public GitBlitModule(@PluginName final String name,
+      @GerritServerConfig final Config gerritConfig, final SitePaths sitePaths) {
+    log.info("Create GitBlitModule with name='" + name);
+  }
+
+  @Override
+  protected void configureServlets() {
+    log.info("Configuring servlet and filtes");
+    bind(IUserService.class).to(GerritToGitBlitUserService.class);
+    bind(GerritGitBlit.class);
+
+    serve("/pages/*").with(WrappedPagesServlet.class);
+    serve("/feed/*").with(WrappedSyndicationServlet.class);
+
+    filter("/*").through(GerritWicketFilter.class);
+    filter("/pages/*").through(WrappedPagesFilter.class);
+    filter("/feed/*").through(WrappedSyndicationFilter.class);
+  }
+}

tomaswolf/gerrit-gitblit-plugin

src/main/java/com/googlesource/gerrit/plugins/gitblit/GitBlitModule.java
@@ -1,0 +1,51 @@
+// Copyright (C) 2012 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.gitblit;
+
+import org.eclipse.jgit.lib.Config;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.gitblit.IUserService;
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+import com.google.inject.servlet.ServletModule;
+import com.googlesource.gerrit.plugins.gitblit.app.GerritGitBlit;
+import com.googlesource.gerrit.plugins.gitblit.auth.GerritToGitBlitUserService;
+
+public class GitBlitModule extends ServletModule {
+  private static final Logger log = LoggerFactory.getLogger(GitBlitModule.class);
+
+  @Inject
+  public GitBlitModule(@PluginName final String name,
+      @GerritServerConfig final Config gerritConfig, final SitePaths sitePaths) {
+    log.info("Create GitBlitModule with name='" + name);
+  }
+
+  @Override
+  protected void configureServlets() {
+    log.info("Configuring servlet and filters");
+    bind(IUserService.class).to(GerritToGitBlitUserService.class);
+    bind(GerritGitBlit.class);
+
+    serve("/pages/*").with(WrappedPagesServlet.class);
+    serve("/feed/*").with(WrappedSyndicationServlet.class);
+
+    filter("/*").through(GerritWicketFilter.class);
+    filter("/pages/*").through(WrappedPagesFilter.class);
+    filter("/feed/*").through(WrappedSyndicationFilter.class);
+  }
+}