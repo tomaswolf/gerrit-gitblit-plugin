8487b4239068913e559268aeb047242614564bfb
Tom <tw201207@gmail.com>
2014-11-05 14:11:49 +0100

    Comment changes

--- src/main/java/com/googlesource/gerrit/plugins/gitblit/app/FixedUrlXSSFilter.java
+++ src/main/java/com/googlesource/gerrit/plugins/gitblit/app/FixedUrlXSSFilter.java
@@ -27,11 +27,12 @@ public class FixedUrlXSSFilter extends JSoupXssFilter {
 		// We know that this is only ever called through none() from the GitblitParamUrlCodingStrategy. This exists only to strip HTML from URL
 		// request parameters. The cleaner is one that strips all HTML tags. I'm not entirely sure this is a good thing to do. What if I have a
 		// file named "<body>.txt" in my git repository?? GitBlit uses filenames quite often as URL parameters, so it won't be able to deal
-		// with such files! Care has to be taken when such file names are written to HTML again, but on input?
+		// with such files! Care has to be taken when such file names are written to HTML again, but on input? Actually, I think this should
+		// not be done at all here.
 		//
 		// Anyway, the problem here is that the final html() call at the end will encode some characters as HTML character entities, which then
 		// breaks a lot of things later on. We must undo that (after all, we already know that all HTML has been stripped). I just hope the rest
-		// of GitBlit knows what it does when it writes pathes to HTML and properly encodes them then.
+		// of GitBlit knows what it does when it writes paths to HTML and properly encodes them then.
 		Document unsafe = Jsoup.parse(input);
 		Document safe = cleaner.clean(unsafe);
 		// Let's restrict JSoup to only encode lt, gt, amp, apos, and quot. Less clean-up work to do later on.
